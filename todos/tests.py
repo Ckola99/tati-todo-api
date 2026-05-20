2ce1b72d67c6b6b09b1d0ef5968527d8fad2cf6f
Christopher kola <Christopherkola@gmail.com>
2026-05-20 08:19:09 +0200

    [TEST]: add DRF APITestCase to test Todo creation

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Todo
 
 # Create your tests here.
+class TododAPITests(APITestCase):
+
+	def setUp(self):
+		"""Runs before every test to create a sample todo"""
+		self.todo = Todo.objects.create(
+			title="Test todo",
+			description="Test description text",
+			is_completed=False
+		)
+		self.list_url = reverse('todo-list')
+		self.detail_url = reverse('todo-detail', kwargs={'pk':self.todo.pk})
+
+	def test_create_todo(self):
+		"""This test checks whether we can correctly post a todo item"""
+		todoData = {"title": "New todo title", "description": "New description text"}
+		response = self.client.post(self.list_url, todoData, format='json')
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		self.assertEqual(Todo.objects.count(), 2)
