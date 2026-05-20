14d6c3d99d7546ff6ee8eb9acee9e3aef8f497f7
Christopher kola <Christopherkola@gmail.com>
2026-05-20 09:55:49 +0200

    [CHORE]: add Docker scaffolding (Dockerfile, .dockerignore, docker-compose) [FEAT]: add deployment dependencies (gunicorn, packaging, whitenoise) [FEAT]: configure WhiteNoise and STATIC_ROOT for static files

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "todo_project.urls"
@@ -133,3 +135,5 @@ LOGGING = {
         },
     },
 }
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
