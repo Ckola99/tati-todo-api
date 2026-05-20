a84763788b04382887bd73955b713e7744837683
Christopher kola <Christopherkola@gmail.com>
2026-05-20 08:36:20 +0200

    [TEST]: add test_retrieve_todo for retrieving single todo item

--- todos/tests.py
+++ todos/tests.py
@@ -28,3 +28,10 @@ class TodoAPITests(APITestCase):
 		response = self.client.get(self.list_url)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(len(response.data), 1)
+
+	def test_retrieve_todo(self):
+		"""This test checks whether it is able to retieve one single todo item"""
+		response = self.client.get(self.detail_url)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data['title'], "Test todo")
+	
