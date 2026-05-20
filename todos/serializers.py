c0591ec17cc668d0939b750394559d6a77c62877
Christopher kola <Christopherkola@gmail.com>
2026-05-20 07:43:21 +0200

    [FEAT]: scaffold Django todo API project with DRF and SQLite

---
+++ todos/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+	title = serializers.CharField(min_length=3, max_length=100)
+	description = serializers.CharField(min_length=3)
+
+	class Meta:
+		model = Todo
+		fields = ["id", "title", "description", "is_completed", "created_at", "updated_at"]
+		read_only_fields = ['id', 'created_at', 'updated_at']
---
+++ todos/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+	queryset = Todo.objects.all()
+	serializer_class = TodoSerializer
+
+	def create(self, request, *args, **kwargs):
+		try:
+			response = super().create(request, *args, **kwargs)
+			logger.info(f"Todo created with id:{response.data['id']}")
+			return response
+		except Exception as e:
+			logger.error(f"Failed to create todo item: {e}")
+			raise
+
+	def update(self, request, *args, **kwargs):
+		todo_id = kwargs.get('pk')
+		try:
+			response = super().update(request, *args, **kwargs)
+			logger.info(f"Todo with id:{response.data['id']} updated")
+			return response
+		except Exception as e:
+			logger.error(f"Failed to update todo item with id:{todo_id}: {e}")
+			raise
+
+	def destroy(self, request, *args, **kwargs):
+		todo_id = kwargs.get('pk')
+		try:
+			response = super().destroy(request, *args, **kwargs)
+			logger.info(f"Todo with id:{todo_id} deleted")
+			return response
+		except Exception as e:
+			logger.error(f"Failed to delete todo item with id:{todo_id}: {e}")
+			raise
